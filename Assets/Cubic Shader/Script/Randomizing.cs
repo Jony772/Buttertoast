c42217e3d84cc780e3ec059b57fa09464a04d81b
yonatan ilan <ilanyonatan772@gmail.com>
2021-08-23 00:08:10 +0300

    1

---
+++ Assets/Cubic Shader/Script/Randomizing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Randomizing : MonoBehaviour
+
+{
+    public MeshRenderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<MeshRenderer>();
+        rend.material.SetFloat("Vector1_randomizer", Random.Range(0f, 1f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
