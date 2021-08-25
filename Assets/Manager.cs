43081e7f19bb5770c73c182388f17218ebdf8091
yonatan ilan <ilanyonatan772@gmail.com>
2021-08-25 15:56:36 +0300

    Raging Works!

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -9,12 +9,19 @@ public class Manager : MonoBehaviour
     public GameObject  TextProGirl;
     public GameObject butterPassText;
     public GameObject butterPlate;
-    private int passCount;
+    public GameObject rageText;
+    public static int passCount = 0;
+
+    public GameObject calmButter;
+    public GameObject ragedButter;
+    public bool isRaged = false;
 
 
     void Start()
     {
         InvokeRepeating("ButterPassGirl", 5, 4);
+        InvokeRepeating("ButterPassDude", 8, 6);
+
     }
 
     void Update()
@@ -36,20 +43,31 @@ public class Manager : MonoBehaviour
             ButterPass.girl = false;
         } */
   
+        if(passCount > 10)
+        {
+            butterPassText.SetActive(false);
+            rageText.SetActive(true);
+        }
 
+        if (Input.GetKeyDown(KeyCode.Space) && passCount > 10)
+        {
+            calmButter.SetActive(false);
+            ragedButter.SetActive(true);
+            isRaged = true;
+        }
 
     }
 
     public void ButterPassGirl()
     {
         TextProGirl.SetActive(true);
-        butterPassText.SetActive(true);
+        //butterPassText.SetActive(true);
 
     }
 
     public void ButterPassDude()
     {
-
+        TextProDude.SetActive(true);
     }
 
 }
--- Assets/dudeCollid.cs
+++ Assets/dudeCollid.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class girlCollid : MonoBehaviour
+public class dudeCollid : MonoBehaviour
 {
     public GameObject  TextProDude;
-    public GameObject  TextProGirl;
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
@@ -20,7 +17,7 @@ public class girlCollid : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        TextProGirl.SetActive(false);
+        TextProDude.SetActive(false);
         Manager.passCount = Manager.passCount + 1;
     }
 }
